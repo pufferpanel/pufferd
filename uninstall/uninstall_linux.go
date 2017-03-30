fca83bacd9356565ab02f4f9d0ddcfed8c2a6792
Andrea <andry25.9.99@gmail.com>
2017-03-30 21:37:49 +0200

    Improved uninstalling function
    Now the function uninstall based on the config file

--- uninstall/uninstall_linux.go
+++ uninstall/uninstall_linux.go
@@ -64,17 +64,29 @@ func deleteFiles(){
 	if err != nil {
 		logging.Error("Error disabling pufferd service, is it installed?", err)
 	}
-	
-	err = os.RemoveAll("/etc/pufferd")
-	if err != nil {
-		logging.Error("Error deleting pufferd configuration, stored in \"/etc/pufferd\"", err)
-	}
 
 	//delete service
 	err :=os.Remove("/etc/systemd/system/pufferd.service")
 	if err != nil{
 		logging.Error("Error deleting the pufferd service file:", err)
 	}
+
+	err = os.RemoveAll(config.Get("serverfolder"))
+	if err != nil {
+		logging.Error("Error deleting pufferd server folder, stored in %s",config.Get("serverfolder") , err)
+	}
+
+	err = os.RemoveAll(config.Get("templatefolder"))
+	if err != nil {
+		logging.Error("Error deleting pufferd template folder, stored in %s",config.Get("templatefolder") , err)
+	}
+
+	err = os.RemoveAll(config.Get("datafolder"))
+	if err != nil {
+		logging.Error("Error deleting pufferd data folder, stored in %s",config.Get("datafolder") , err)
+	}
+
+
 }
 
 
