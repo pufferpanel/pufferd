3d87a82f13c92af1807c574ee0960510e5f35b67
Andrea <andry25.9.99@gmail.com>
2017-04-01 11:00:34 +0200

    Windows uninstaller

--- uninstall/uninstall_windows.go
+++ uninstall/uninstall_windows.go
@@ -2,9 +2,27 @@ package uninstall
 
 import (
 	"github.com/pufferpanel/pufferd/logging"
+	"os"
 )
 
 
 func StartProcess(){
-	logging.Error("No configured service uninstaller for this OS")
+	deleteFiles()
+}
+
+func deleteFiles(){
+err = os.RemoveAll(config.Get("serverfolder"))
+	if err != nil {
+		logging.Error("Error deleting pufferd server folder, stored in %s",config.Get("serverfolder") , err)
+	}
+
+	err = os.RemoveAll(config.Get("templatefolder"))
+	if err != nil {
+		logging.Error("Error deleting pufferd template folder, stored in %s",config.Get("templatefolder") , err)
+	}
+
+	err = os.RemoveAll(config.Get("datafolder"))
+	if err != nil {
+		logging.Error("Error deleting pufferd data folder, stored in %s",config.Get("datafolder") , err)
+	}
 }
