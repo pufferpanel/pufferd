bce6f5c44dbf6f307d3f370b51db7b315a27711f
Andrea <andry25.9.99@gmail.com>
2017-04-12 22:05:43 +0200

    Update uninstaller_linux.go

--- uninstaller/uninstaller_linux.go
+++ uninstaller/uninstaller_linux.go
@@ -73,17 +73,17 @@ func deleteFiles() {
 
 	err = os.RemoveAll(config.Get("serverfolder"))
 	if err != nil {
-		logging.Error("Error deleting pufferd server folder, stored in " + config.Get("serverfolder") + err)
+		logging.Error("Error deleting pufferd server folder, stored in " + config.Get("serverfolder"), err)
 	}
 
 	err = os.RemoveAll(config.Get("templatefolder"))
 	if err != nil {
-		logging.Error("Error deleting pufferd template folder, stored in " + config.Get("templatefolder") + err)
+		logging.Error("Error deleting pufferd template folder, stored in " + config.Get("templatefolder"), err)
 	}
 
 	err = os.RemoveAll(config.Get("datafolder"))
 	if err != nil {
-		logging.Error("Error deleting pufferd data folder, stored in " + config.Get("datafolder") + err)
+		logging.Error("Error deleting pufferd data folder, stored in " + config.Get("datafolder"), err)
 	}
 
 }
