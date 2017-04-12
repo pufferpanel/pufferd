f68027dc65e23dcbfa013adb68f714c4b3df41b9
Andrea <andry25.9.99@gmail.com>
2017-04-12 22:05:59 +0200

    Update uninstaller_windows.go

--- uninstaller/uninstaller_windows.go
+++ uninstaller/uninstaller_windows.go
@@ -13,16 +13,16 @@ func StartProcess() {
 func deleteFiles() {
 	err := os.RemoveAll(config.Get("serverfolder"))
 	if err != nil {
-		logging.Error("Error deleting pufferd server folder, stored in " + config.Get("serverfolder") + err)
+		logging.Error("Error deleting pufferd server folder, stored in " + config.Get("serverfolder"), err)
 	}
 
 	err = os.RemoveAll(config.Get("templatefolder"))
 	if err != nil {
-		logging.Error("Error deleting pufferd template folder, stored in " + config.Get("templatefolder") + err)
+		logging.Error("Error deleting pufferd template folder, stored in " + config.Get("templatefolder"), err)
 	}
 
 	err = os.RemoveAll(config.Get("datafolder"))
 	if err != nil {
-		logging.Error("Error deleting pufferd data folder, stored in " + config.Get("datafolder") + err)
+		logging.Error("Error deleting pufferd data folder, stored in " + config.Get("datafolder"), err)
 	}
 }
